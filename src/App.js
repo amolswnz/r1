929cc7d0a6e4231d5627b64966f3d7d6f2b50374
Amol <amolsw.nz@gmail.com>
2018-12-14 13:58:34 +1300

    deploying

--- src/App.js
+++ src/App.js
@@ -2,14 +2,23 @@ import React, { Component } from "react";
 import "./App.css";
 import ExChart from "./components/ExChart";
 import { Row, Col } from "antd";
+import { DatePicker } from 'antd';
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
 
 class App extends Component {
+  onChange = (date, dateString) => {
+    console.log(date, dateString);
+  }
+
   render() {
     return (
       <div>
-        <Row>
-          <Col>
+        <Row  type="flex" >
+          <Col span={12}>
             <ExChart />
+          </Col>
+          <Col span={12}>
+            <RangePicker onChange={this.onChange} />
           </Col>
         </Row>
       </div>
