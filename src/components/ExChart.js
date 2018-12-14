de591b778f57a68b15ba3fc1499b7a3699169dae
Amol <amolsw.nz@gmail.com>
2018-12-14 13:25:56 +1300

    three bar charts

--- src/components/ExChart.js
+++ src/components/ExChart.js
@@ -17,13 +17,13 @@ class ExChart extends React.Component {
     const max=3333;  
 
     const data = [
-      { name: "Page A", uv: Math.floor(Math.random() * (+max - +min)), pv: Math.floor(Math.random() * (+max - +min))},
-      { name: "Page B", uv: Math.floor(Math.random() * (+max - +min)), pv: Math.floor(Math.random() * (+max - +min))},
-      { name: "Page C", uv: Math.floor(Math.random() * (+max - +min)), pv: Math.floor(Math.random() * (+max - +min))},
-      { name: "Page D", uv: Math.floor(Math.random() * (+max - +min)), pv: Math.floor(Math.random() * (+max - +min))},
-      { name: "Page E", uv: Math.floor(Math.random() * (+max - +min)), pv: Math.floor(Math.random() * (+max - +min))},
-      { name: "Page F", uv: Math.floor(Math.random() * (+max - +min)), pv: Math.floor(Math.random() * (+max - +min))},
-      { name: "Page G", uv: Math.floor(Math.random() * (+max - +min)), pv: Math.floor(Math.random() * (+max - +min))}
+      { name: "Page A", uv:  Math.floor(Math.random()*(max-min+1)+min),  xx:  Math.floor(Math.random()*(max-min+1)+min), pv:  Math.floor(Math.random()*(max-min+1)+min)},
+      { name: "Page B", uv:  Math.floor(Math.random()*(max-min+1)+min),  xx:  Math.floor(Math.random()*(max-min+1)+min), pv:  Math.floor(Math.random()*(max-min+1)+min)},
+      { name: "Page C", uv:  Math.floor(Math.random()*(max-min+1)+min),  xx:  Math.floor(Math.random()*(max-min+1)+min), pv:  Math.floor(Math.random()*(max-min+1)+min)},
+      { name: "Page D", uv:  Math.floor(Math.random()*(max-min+1)+min),  xx:  Math.floor(Math.random()*(max-min+1)+min), pv:  Math.floor(Math.random()*(max-min+1)+min)},
+      { name: "Page E", uv:  Math.floor(Math.random()*(max-min+1)+min),  xx:  Math.floor(Math.random()*(max-min+1)+min), pv:  Math.floor(Math.random()*(max-min+1)+min)},
+      { name: "Page F", uv:  Math.floor(Math.random()*(max-min+1)+min),  xx:  Math.floor(Math.random()*(max-min+1)+min), pv:  Math.floor(Math.random()*(max-min+1)+min)},
+      { name: "Page G", uv:  Math.floor(Math.random()*(max-min+1)+min),  xx:  Math.floor(Math.random()*(max-min+1)+min), pv:  Math.floor(Math.random()*(max-min+1)+min)}
     ];
     return (
       // <Row type="flex" justify="center" align="top">
@@ -42,6 +42,7 @@ class ExChart extends React.Component {
               <Legend />
               <Bar dataKey="pv" fill="#8884d8" />
               <Bar dataKey="uv" fill="#82ca9d" />
+              <Bar dataKey="xx" fill="#de3e32" />
             </BarChart>
           </ResponsiveContainer>
       //   </Col>
