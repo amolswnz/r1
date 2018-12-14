faea0dc48af5cfd17e1fa7bc242ae2927fb3326c
Amol Wankhede <amolsw.nz@gmail.com>
2018-12-14 14:08:39 +1300

    Updates

---
+++ precache-manifest.0026b78a578655f88e9bfbc22183ee2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "390f56e3741eb0ef8be0",
+    "url": "/r1/static/css/main.2e41d422.chunk.css"
+  },
+  {
+    "revision": "390f56e3741eb0ef8be0",
+    "url": "/r1/static/js/main.390f56e3.chunk.js"
+  },
+  {
+    "revision": "6dc7f9dc59e8ec2a95bd",
+    "url": "/r1/static/css/1.6a2364e6.chunk.css"
+  },
+  {
+    "revision": "6dc7f9dc59e8ec2a95bd",
+    "url": "/r1/static/js/1.6dc7f9dc.chunk.js"
+  },
+  {
+    "revision": "ef5df5e38d7125ecfddf",
+    "url": "/r1/static/js/runtime~main.ef5df5e3.js"
+  },
+  {
+    "revision": "00e8af0a44915273fd23b49971449a5b",
+    "url": "/r1/index.html"
+  }
+];
